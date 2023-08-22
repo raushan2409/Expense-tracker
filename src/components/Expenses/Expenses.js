4bd08221d592124c4391332a043355c17a686322
Raushan Singh <88944944+raushan2409@users.noreply.github.com>
2023-08-22 11:16:20 +0530

    Update Expenses.js
    added expenseFilter (but not the whole functionality) only expensefilter.js n css

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
-import React from 'react';
+import React,{useState} from 'react';
+import ExpenseFilter from './ExpenseFilter';
 
-function Expenses(props) {
+const Expenses = (props) =>{
+
+  const [filteredYear,setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
     <Card className="expenses">
+    <ExpenseFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
       {props.itemhai.map(
         expense => <ExpenseItem
         date={expense.date}
