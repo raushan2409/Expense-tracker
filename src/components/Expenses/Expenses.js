c57f7edc4ab7a8cefc3af9c4f7d5a1a0df7754ee
Raushan Singh <88944944+raushan2409@users.noreply.github.com>
2023-08-22 20:27:40 +0530

    Update Expenses.js
    Filter function added "Only single Expense here. Please add more..."

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,30 +1,34 @@
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
-import React,{useState} from 'react';
-import ExpenseFilter from './ExpenseFilter';
+import Card from "../UI/Card";
+import "./Expenses.css";
+import React, { useState } from "react";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
-const Expenses = (props) =>{
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const [filteredYear,setFilteredYear] = useState('2020');
-
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
 
   return (
+    <li>
     <Card className="expenses">
-    <ExpenseFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
-      {props.items.map(
-        expense => <ExpenseItem
-        key={expense.id}
-        date={expense.date}
-        title={expense.title}
-        amount={expense.amount}
-        />
-      )}
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesList items={filteredExpenses} />
+
+      
     </Card>
+    </li>
   );
-}
+};
 
 export default Expenses;
