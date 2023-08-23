ba038e34e6b0edc220e32d6b9aee6cc2c29dee5d
Raushan Singh <88944944+raushan2409@users.noreply.github.com>
2023-08-24 00:00:44 +0530

    Update ExpensesList.js
    removing if item.length is 1

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -6,9 +6,7 @@ const ExpensesList = props =>{
     if(props.items.length===0){
       return <h2 className='expenses-list__fallback'>Found no expenses.</h2>
     }
-    if(props.items.length===1){
-      return <h2 className='expenses-list__fallback'>Only single Expense here. Please add more...</h2>
-    }
+    
     return <ul className='expenses-list'>
     {props.items.map((expense) => (
         <ExpenseItem
